3788b42c3a0e0b31697374c72ab409ac9b50c7e5
Avocadorable0 <120171050+Avocadorable0@users.noreply.github.com>
2023-02-07 18:14:04 +0300

    Update content.php

--- application/views/template/content.php
+++ application/views/template/content.php
@@ -68,7 +68,7 @@
               <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
               <div class="card-body">
                 <h4 class="card-title">
-                  <a href="#"><?php echo $produit['nomProduit']; ?></a>
+                  <a href="#"><?php echo $produit['nom']; ?></a>
                 </h4>
                 <h5>$24.99</h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
