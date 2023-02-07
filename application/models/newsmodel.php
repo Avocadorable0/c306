5e2c6b0927188de23abc98b49ee77a7db8d91977
Faly-Rajaonah <itufaly@gmail.com>
2023-02-07 22:45:13 +0300

    newsmodel.php

--- application/models/newsmodel.php
+++ application/models/newsmodel.php
@@ -11,6 +11,18 @@ class newsModel extends CI_Model {
         }
         return $result;
     }
+
+    public function listproduct(){
+        $sql = "select utilisateur.idutilisateur,utilisateur.nom as proprietaire,produit.nom as produit, prix from EXCHANGE
+                join produit on produit.idproduit=EXCHANGE.idproduit
+                join utilisateur on utilisateur.idutilisateur=EXCHANGE.idutilisateur";
+        $squery = $this->db->query($sql);
+        foreach($squery->result_array() as $row){
+            $result[] = $row;
+        }
+        return $result;
+    }
+
     public function conekta($Email,$Mdp){
         $sql="select * from utilisateur where gmail=%s and mdp = %s";
         $sql1=sprintf($sql,$this->db->escape($Email),$this->db->escape($Mdp));
