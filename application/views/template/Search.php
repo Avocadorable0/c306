abebca4d2c70bf278d94a7e04ffda6f7f45a1599
Faly-Rajaonah <itufaly@gmail.com>
2023-02-08 14:19:13 +0300

    Search

---
+++ application/views/template/Search.php
@@ -0,0 +1,38 @@
+    <!-- Page Content -->
+    <div class="container">
+
+<div class="row">
+    <h1 class="my-4">Resultat de recherches</h1>
+    <!-- Eto no mipotra ny liste ny produit -->
+    <?php for ($i=0; $i < 8 ; $i++) { ?>
+
+      <div class="col-lg-4 col-md-6 mb-4">
+
+        <div class="card h-100">
+          <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+          <div class="card-body">
+            <h4 class="card-title">
+              <a href="#">Nike Air Max One ?></a>
+            </h4>
+            <h5>$12.50</h5>
+            <h3  class="card-text">User: Fanilo</h3>
+            <p class="card-text">Tres bon tennis jamais porte</p>
+            <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Clicker pour faire l'echange</a>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+          </div>
+        </div>
+
+      </div> 
+    <?php    } ?>
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.col-lg-9 -->
+
+</div>
+<!-- /.row -->
+
+</div>
