70f0f3d695cea15777eb6fcec3871e13d060778d
Avocadorable0 <120171050+Avocadorable0@users.noreply.github.com>
2023-02-08 15:16:13 +0300

    finalisation

--- application/views/template/header2.php
+++ application/views/template/header2.php
@@ -27,29 +27,9 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <form action="research" method="post">
-        <input type="text" name="mot">
-        <select name="categorie">
-          <option>Vetement</option>
-          <option>Objet</option>
-          <option>Technologie</option>
-        </select>
-        <input type="submit" value="Search">
-      </form>
       </div>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
           <li class="nav-item">
             <a class="nav-link" href="deconexion">Log out</a>
           </li>
